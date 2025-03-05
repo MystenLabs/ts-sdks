e40bb1a488bb3357f20a4e52f59eab47c63aae55
Pavlos Chrysochoidis <pavlos@mystenlabs.com>
2025-03-05 10:17:29 -0700

    specific exports

--- packages/enoki-connect/src/index.ts
+++ packages/enoki-connect/src/index.ts
@@ -1,5 +1,13 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-export * from './wallet/index.js';
-export * from './wallet/channel/index.js';
+export {
+	EnokiConnectWallet,
+	registerEnokiConnectWallet,
+	type SupportedNetwork,
+} from './wallet/index.js';
+export {
+	type EnokiConnectRequest,
+	type EnokiConnectResponse,
+	EnokiConnectHost,
+} from './wallet/channel/index.js';
