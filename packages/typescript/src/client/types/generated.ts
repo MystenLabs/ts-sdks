1f5ccab40d168f3e9633b7c9a9b621d756d3f850
vladilen11 <vladilenaksana@163.com>
2025-03-08 16:09:58 +0800

    update SuiMoveNormalizedEnum.variants type to vector

--- packages/typescript/src/client/types/generated.ts
+++ packages/typescript/src/client/types/generated.ts
@@ -1004,9 +1004,7 @@ export interface SuiMoveModuleId {
 export interface SuiMoveNormalizedEnum {
 	abilities: SuiMoveAbilitySet;
 	typeParameters: SuiMoveStructTypeParameter[];
-	variants: {
-		[key: string]: SuiMoveNormalizedField[];
-	};
+	variants: [string, SuiMoveNormalizedField[]][];
 }
 export interface SuiMoveNormalizedField {
 	name: string;
