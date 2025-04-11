5335eeda6aabb903bb647348cd080fd00a56d16f
William Robertson <williamrobertson@mystenlabs.com>
2025-04-11 11:33:59 -0400

    ee

--- packages/dapp-kit-next/src/store/state.ts
+++ packages/dapp-kit-next/src/store/state.ts
@@ -2,105 +2,34 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import type { Wallet, WalletAccount, WalletWithRequiredFeatures } from '@mysten/wallet-standard';
-import { persistentAtom } from '@nanostores/persistent';
-import { map } from 'nanostores';
+import { map, mapCreator, MapStore } from 'nanostores';
 
-import { getRegisteredWallets, getWalletUniqueIdentifier } from '../utils/walletUtils.js';
+import { getRegisteredWallets } from '../utils/walletUtils.js';
 
 export type DappKitStateOptions = {
-	autoConnectEnabled: boolean;
 	preferredWallets: string[];
 	walletFilter: (wallet: WalletWithRequiredFeatures) => boolean;
 	storageKey: string;
 };
 
-export type WalletConnectionStatus = 'disconnected' | 'connecting' | 'connected';
-export type AutoConnectStatus = 'disabled' | 'idle' | 'attempted';
-
 export type DappKitStoreState = {
 	wallets: WalletWithRequiredFeatures[];
-	accounts: readonly WalletAccount[];
 	currentWallet: WalletWithRequiredFeatures | null;
-	currentAccount: WalletAccount | null;
-	connectionStatus: WalletConnectionStatus;
-	supportedIntents: string[];
 };
 
-export type LastConnectionStoreState = {
-	walletName?: string | null;
-	accountAddress?: string | null;
-};
+const DAppKitStore = mapCreator<DappKitStoreState>((store, id) => {
+	store.set({ id, wallets: [], currentWallet: null });
+});
+
+const GlobalStore = DAppKitStore('global-dapp-kit-store');
 
 export function createState({ preferredWallets, walletFilter, storageKey }: DappKitStateOptions) {
 	const $state = map<DappKitStoreState>({
 		wallets: getRegisteredWallets(preferredWallets, walletFilter),
-		accounts: [] as WalletAccount[],
 		currentWallet: null,
-		currentAccount: null,
-		connectionStatus: 'disconnected',
-		supportedIntents: [],
 	});
 
-	// TODO: Make storageKey optional and default to just a plain atom if not provided
-	const $recentConnection = persistentAtom<LastConnectionStoreState>(
-		storageKey,
-		{
-			walletName: null,
-			accountAddress: null,
-		},
-		{
-			encode: JSON.stringify,
-			decode: JSON.parse,
-		},
-	);
-
 	const actions = {
-		setConnectionStatus(connectionStatus: WalletConnectionStatus) {
-			$state.setKey('connectionStatus', connectionStatus);
-		},
-		setWalletConnected(
-			wallet: WalletWithRequiredFeatures,
-			connectedAccounts: readonly WalletAccount[],
-			selectedAccount: WalletAccount | null,
-			supportedIntents: string[] = [],
-		) {
-			$state.set({
-				...$state.get(),
-				accounts: connectedAccounts,
-				currentWallet: wallet,
-				currentAccount: selectedAccount,
-				connectionStatus: 'connected',
-				supportedIntents,
-			});
-
-			$recentConnection.set({
-				walletName: getWalletUniqueIdentifier(wallet),
-				accountAddress: selectedAccount?.address,
-			});
-		},
-		setWalletDisconnected() {
-			$state.set({
-				...$state.get(),
-				accounts: [],
-				currentWallet: null,
-				currentAccount: null,
-				connectionStatus: 'disconnected',
-				supportedIntents: [],
-			});
-
-			$recentConnection.set({
-				walletName: null,
-				accountAddress: null,
-			});
-		},
-		setAccountSwitched(selectedAccount: WalletAccount) {
-			$state.setKey('currentAccount', selectedAccount);
-
-			$recentConnection.set({
-				...$recentConnection.get(),
-				accountAddress: selectedAccount.address,
-			});
-		},
 		setWalletRegistered(updatedWallets: WalletWithRequiredFeatures[]) {
 			$state.setKey('wallets', updatedWallets);
 		},
@@ -118,31 +47,14 @@ export function createState({ preferredWallets, walletFilter, storageKey }: Dapp
 					connectionStatus: 'disconnected',
 					supportedIntents: [],
 				});
-
-				$recentConnection.set({
-					walletName: null,
-					accountAddress: null,
-				});
 			} else {
 				$state.setKey('wallets', updatedWallets);
 			}
-		},
-		updateWalletAccounts(accounts: readonly WalletAccount[]) {
-			const { currentAccount, ...state } = $state.get();
-
-			$state.set({
-				...state,
-				accounts,
-				currentAccount:
-					(currentAccount && accounts.find(({ address }) => address === currentAccount.address)) ||
-					accounts[0],
-			});
 		},
 	};
 
 	return {
 		$state,
-		$recentConnection,
 		actions,
 	};
 }
