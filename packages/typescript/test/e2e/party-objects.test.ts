c64b86790753d3c7293a7dbc3c7d09b5d5b29500
Michael Hayes <michael.hayes@mystenlabs.com>
2025-07-25 13:30:31 -0700

    handle party transfer ptb command

--- packages/typescript/test/e2e/party-objects.test.ts
+++ packages/typescript/test/e2e/party-objects.test.ts
@@ -70,4 +70,66 @@ describe('Party Objects', () => {
 
 		expect(returnEffects!.status.status).toBe('success');
 	});
+
+	it('should correctly handle party objects only used in ptb commands', async () => {
+		const createPartyTxn = new Transaction();
+		createPartyTxn.setSender(await toolbox.address());
+
+		const party = createPartyTxn.moveCall({
+			target: '0x2::party::single_owner',
+			arguments: [createPartyTxn.pure.address(await toolbox.address())],
+		});
+
+		createPartyTxn.moveCall({
+			target: '0x2::transfer::public_party_transfer',
+			typeArguments: ['0x2::coin::Coin<0x2::sui::SUI>'],
+			arguments: [coinWithBalance({ balance: 1 }), party],
+		});
+
+		const { digest } = await toolbox.client.signAndExecuteTransaction({
+			transaction: await createPartyTxn.build({
+				client: toolbox.client,
+			}),
+			signer: toolbox.keypair,
+			options: {
+				showEffects: true,
+			},
+		});
+
+		const { effects } = await toolbox.client.waitForTransaction({
+			digest,
+			options: {
+				showEffects: true,
+			},
+		});
+
+		const partyCoin = effects!.created![0].reference;
+
+		const returnTx = new Transaction();
+		returnTx.setSender(await toolbox.address());
+		returnTx.transferObjects(
+			[
+				returnTx.sharedObjectRef({
+					objectId: partyCoin.objectId,
+					mutable: true,
+					initialSharedVersion: partyCoin.version,
+				}),
+			],
+			toolbox.keypair.getPublicKey().toSuiAddress(),
+		);
+
+		const { digest: returnDigest } = await toolbox.client.signAndExecuteTransaction({
+			transaction: await returnTx.build({ client: toolbox.client }),
+			signer: toolbox.keypair,
+		});
+
+		const { effects: returnEffects } = await toolbox.client.waitForTransaction({
+			digest: returnDigest,
+			options: {
+				showEffects: true,
+			},
+		});
+
+		expect(returnEffects!.status.status).toBe('success');
+	});
 });
