75d2fcc1abe8765d3ff827eab237193b8614af2d
Michael Hayes <michael.hayes@mystenlabs.com>
2025-08-08 20:31:24 -0400

    signing client

--- packages/typescript/src/experimental/index.ts
+++ packages/typescript/src/experimental/index.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import type { IntentScope } from '../cryptography/intent.js';
+import type { SignAndExecuteOptions, Signer } from '../cryptography/keypair.js';
 import { Experimental_BaseClient } from './client.js';
 import type { ClientWithCoreApi, Experimental_CoreClientOptions } from './core.js';
 import { Experimental_CoreClient } from './core.js';
@@ -22,3 +24,66 @@ export {
 };
 
 export { ClientCache, type ClientCacheOptions } from './cache.js';
+
+export class SigningClient extends Experimental_BaseClient implements Signer, ClientWithCoreApi {
+	#client: ClientWithCoreApi;
+	#signer: Signer;
+
+	constructor({
+		signer,
+		client,
+		...options
+	}: { signer: Signer; client: ClientWithCoreApi } & Experimental_SuiClientTypes.SuiClientOptions) {
+		super({
+			base: client.base,
+			...options,
+		});
+
+		this.#client = client;
+		this.#signer = signer;
+	}
+
+	get core(): Experimental_CoreClient {
+		return this.#client.core;
+	}
+
+	sign(bytes: Uint8Array) {
+		return this.#signer.sign(bytes);
+	}
+
+	getKeyScheme() {
+		return this.#signer.getKeyScheme();
+	}
+
+	getPublicKey() {
+		return this.#signer.getPublicKey();
+	}
+
+	signWithIntent(bytes: Uint8Array, intent: IntentScope) {
+		return this.#signer.signWithIntent(bytes, intent);
+	}
+
+	signPersonalMessage(bytes: Uint8Array) {
+		return this.#signer.signPersonalMessage(bytes);
+	}
+
+	signTransaction(bytes: Uint8Array) {
+		return this.#signer.signTransaction(bytes);
+	}
+
+	toSuiAddress(): string {
+		return this.#signer.toSuiAddress();
+	}
+
+	async signAndExecuteTransaction({
+		transaction,
+	}: Omit<
+		SignAndExecuteOptions,
+		'client'
+	>): Promise<Experimental_SuiClientTypes.TransactionResponse> {
+		return this.#signer.signAndExecuteTransaction({
+			transaction,
+			client: this.#client,
+		});
+	}
+}
