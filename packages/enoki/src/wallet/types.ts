f29c792efcd16bd06726a8f2035f480ee74ec9ff
reslear <12596485+reslear@users.noreply.github.com>
2025-08-20 18:20:04 +0200

    add type

--- packages/enoki/src/wallet/types.ts
+++ packages/enoki/src/wallet/types.ts
@@ -91,7 +91,7 @@ export type RegisterEnokiWalletsOptions = {
 	 * Configuration for each OAuth provider.
 	 */
 	providers: Partial<Record<AuthProvider, AuthProviderOptions>>;
-} & Pick<EnokiWalletOptions, 'apiKey' | 'apiUrl' | 'windowFeatures'> &
+} & Pick<EnokiWalletOptions, 'apiKey' | 'apiUrl' | 'windowFeatures' | 'customAuthorization'> &
 	(
 		| ClientConfig
 		| {
