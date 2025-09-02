12987aef3b5cb76b99c4e3b5e55b5e602fe3ce24
pyncz <pyncz.dev@gmail.com>
2025-09-02 14:56:21 -0500

    chore: codegen run

--- packages/codegen/tests/generated/utils/index.ts
+++ packages/codegen/tests/generated/utils/index.ts
@@ -51,7 +51,7 @@ export function getPureBcsSchema(typeTag: string | TypeTag): BcsType<any> | null
 			}
 
 			if (structTag.module === 'option' && structTag.name === 'Option') {
-				const type = getPureBcsSchema(structTag.typeParams[0]);
+				const type = getPureBcsSchema(structTag.typeParams[0]!);
 				return type ? bcs.vector(type) : null;
 			}
 		}
@@ -127,7 +127,7 @@ export function normalizeMoveArguments(
 			continue;
 		}
 
-		const type = argTypes[i];
+		const type = argTypes[i]!;
 		const bcsType = getPureBcsSchema(type);
 
 		if (bcsType) {
