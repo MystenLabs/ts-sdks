4486f07cddf57afc6a8149989fba3712112bc7ce
Manolis Liolios <manos@mystenlabs.com>
2025-09-08 20:23:54 +0300

    properly handle typetag inputs

--- packages/typescript/src/utils/derived-objects.ts
+++ packages/typescript/src/utils/derived-objects.ts
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import type { TypeTag } from '../bcs/bcs.js';
+import { TypeTagSerializer } from '../bcs/type-tag-serializer.js';
 import { deriveDynamicFieldID } from './dynamic-fields.js';
 
 /**
@@ -12,9 +13,10 @@ export function deriveObjectID(
 	typeTag: typeof TypeTag.$inferInput,
 	key: Uint8Array,
 ) {
+	const typeTagStr = typeof typeTag === 'string' ? typeTag : TypeTagSerializer.tagToString(typeTag);
 	return deriveDynamicFieldID(
 		parentId,
-		`0x2::derived_object::DerivedObjectKey<${typeTag.toString()}>`,
+		`0x2::derived_object::DerivedObjectKey<${typeTagStr}>`,
 		key,
 	);
 }
--- packages/typescript/test/unit/utils/derived-objects.test.ts
+++ packages/typescript/test/unit/utils/derived-objects.test.ts
@@ -3,7 +3,7 @@
 
 import { describe, expect, test } from 'vitest';
 
-import { bcs } from '../../../src/bcs';
+import { bcs, TypeTagSerializer } from '../../../src/bcs';
 import { deriveObjectID } from '../../../src/utils/derived-objects';
 import { normalizeSuiAddress } from '../../../src/utils';
 
@@ -13,9 +13,12 @@ describe('derived object test utils', () => {
 	test('deriveObjectID with primitive type', () => {
 		const key = bcs.vector(bcs.u8()).serialize(new TextEncoder().encode('foo')).toBytes();
 
-		const result = deriveObjectID(normalizeSuiAddress('0x2'), 'vector<u8>', key);
-
-		expect(result).toBe('0xa2b411aa9588c398d8e3bc97dddbdd430b5ded7f81545d05e33916c3ca0f30c3');
+		expect(deriveObjectID(normalizeSuiAddress('0x2'), 'vector<u8>', key)).toBe(
+			'0xa2b411aa9588c398d8e3bc97dddbdd430b5ded7f81545d05e33916c3ca0f30c3',
+		);
+		expect(
+			deriveObjectID(normalizeSuiAddress('0x2'), TypeTagSerializer.parseFromStr('vector<u8>'), key),
+		).toBe('0xa2b411aa9588c398d8e3bc97dddbdd430b5ded7f81545d05e33916c3ca0f30c3');
 	});
 
 	test('deriveObjectID with struct type', () => {
@@ -24,9 +27,16 @@ describe('derived object test utils', () => {
 		});
 		const key = structType.serialize({ value: 1 }).toBytes();
 
-		const result = deriveObjectID('0x2', `0x2::derived_object_tests::DemoStruct`, key);
-
-		expect(result).toBe('0x20c58d8790a5d2214c159c23f18a5fdc347211e511186353e785ad543abcea6b');
+		expect(deriveObjectID('0x2', `0x2::derived_object_tests::DemoStruct`, key)).toBe(
+			'0x20c58d8790a5d2214c159c23f18a5fdc347211e511186353e785ad543abcea6b',
+		);
+		expect(
+			deriveObjectID(
+				'0x2',
+				TypeTagSerializer.parseFromStr('0x2::derived_object_tests::DemoStruct'),
+				key,
+			),
+		).toBe('0x20c58d8790a5d2214c159c23f18a5fdc347211e511186353e785ad543abcea6b');
 	});
 
 	test('deriveObjectID with nested struct type', () => {
@@ -35,8 +45,26 @@ describe('derived object test utils', () => {
 		});
 		const key = structType.serialize({ value: 1 }).toBytes();
 
-		const result = deriveObjectID('0x2', `0x2::derived_object_tests::GenericStruct<u64>`, key);
+		expect(deriveObjectID('0x2', `0x2::derived_object_tests::GenericStruct<u64>`, key)).toBe(
+			'0xb497b8dcf1e297ae5fa69c040e4a08ef8240d5373bbc9d6b686ffbd7dfe04cbe',
+		);
+		expect(
+			deriveObjectID(
+				'0x2',
+				TypeTagSerializer.parseFromStr('0x2::derived_object_tests::GenericStruct<u64>'),
+				key,
+			),
+		).toBe('0xb497b8dcf1e297ae5fa69c040e4a08ef8240d5373bbc9d6b686ffbd7dfe04cbe');
+	});
 
-		expect(result).toBe('0xb497b8dcf1e297ae5fa69c040e4a08ef8240d5373bbc9d6b686ffbd7dfe04cbe');
+	test('deriveObjectID with non-generic primitive type', () => {
+		const key = bcs.U8.serialize(1).toBytes();
+
+		expect(deriveObjectID('0x2', 'u8', key)).toBe(
+			'0x4de7696edddfb592a8dc7c8b66053b1557eb4fa1a9194322562aabf3da9e9239',
+		);
+		expect(deriveObjectID('0x2', TypeTagSerializer.parseFromStr('u8'), key)).toBe(
+			'0x4de7696edddfb592a8dc7c8b66053b1557eb4fa1a9194322562aabf3da9e9239',
+		);
 	});
 });
