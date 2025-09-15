01a603cffe2105a8acdba0ffd5f74130e9becfe7
tomiir <tomas@walletconnect.com>
2025-09-15 11:24:52 +0200

    chore: remove .js from import

--- packages/dapp-kit-next/examples/next-js/simple/app/ClientOnlyConnectButton.tsx
+++ packages/dapp-kit-next/examples/next-js/simple/app/ClientOnlyConnectButton.tsx
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { DAppKitProvider, ConnectButton } from '@mysten/dapp-kit-react';
-import { dAppKit } from './dApp-kit.js';
+import { dAppKit } from './dApp-kit';
 
 export default function ClientOnlyConnectButton() {
 	return (
