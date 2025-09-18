630ea568ffb6076231d0aed5c588549e62173838
jnicoulaud-ledger <102984500+jnicoulaud-ledger@users.noreply.github.com>
2025-09-18 18:29:15 +0200

    Update packages/signers/src/ledger/objects.ts
    Co-authored-by: William Robertson <williamrobertson13@users.noreply.github.com>

--- packages/signers/src/ledger/objects.ts
+++ packages/signers/src/ledger/objects.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import type { Transaction } from '@mysten/sui/dist/cjs/transactions';
+import type { Transaction } from '@mysten/sui/transactions';
 import type { SuiClient } from '@mysten/sui/client';
 import { SuiMoveObject } from './bcs.js';
 
