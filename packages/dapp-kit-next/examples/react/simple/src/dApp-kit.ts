300ec0cca4924d586b930091a079e1da121f2e9c
Michael Hayes <michael.hayes@mystenlabs.com>
2025-12-02 13:09:01 -0800

    restructure docs

--- packages/dapp-kit-next/examples/react/simple/src/dApp-kit.ts
+++ packages/dapp-kit-next/examples/react/simple/src/dApp-kit.ts
@@ -2,14 +2,19 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { createDAppKit } from '@mysten/dapp-kit-react';
-import { getJsonRpcFullnodeUrl, SuiJsonRpcClient } from '@mysten/sui/jsonRpc';
+import { SuiGrpcClient } from '@mysten/sui/grpc';
+
+const GRPC_URLS = {
+	mainnet: 'https://fullnode.mainnet.sui.io:443',
+	testnet: 'https://fullnode.testnet.sui.io:443',
+};
 
 export const dAppKit = createDAppKit({
 	enableBurnerWallet: import.meta.env.DEV,
 	networks: ['mainnet', 'testnet'],
 	defaultNetwork: 'testnet',
 	createClient(network) {
-		return new SuiJsonRpcClient({ network, url: getJsonRpcFullnodeUrl(network) });
+		return new SuiGrpcClient({ network, baseUrl: GRPC_URLS[network] });
 	},
 });
 
