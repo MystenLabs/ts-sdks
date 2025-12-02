c42d35800dfbae9a1004a03e02a9dfc3f6bc6de0
Michael Hayes <michael.hayes@mystenlabs.com>
2025-12-02 11:35:15 -0800

    more pr feedback fixes

--- packages/dapp-kit-next/packages/dapp-kit-react/src/hooks/useWalletConnection.ts
+++ packages/dapp-kit-next/packages/dapp-kit-react/src/hooks/useWalletConnection.ts
@@ -5,13 +5,13 @@ import type { DAppKit, RegisteredDAppKit } from '@mysten/dapp-kit-core';
 import { useStore } from '@nanostores/react';
 import { useDAppKit } from './useDAppKit.js';
 
-export type UseConnectionOptions<TDAppKit extends DAppKit<any>> = {
+export type UseWalletConnectionOptions<TDAppKit extends DAppKit<any>> = {
 	dAppKit?: TDAppKit;
 };
 
-export function useConnection<TDAppKit extends DAppKit = RegisteredDAppKit>({
+export function useWalletConnection<TDAppKit extends DAppKit = RegisteredDAppKit>({
 	dAppKit,
-}: UseConnectionOptions<TDAppKit> = {}) {
+}: UseWalletConnectionOptions<TDAppKit> = {}) {
 	const instance = useDAppKit(dAppKit);
 	return useStore(instance.stores.$connection);
 }
