7de700d0627c96e4e54f7192b0ec7be7ad0ba9f8
hayes-mysten <135670682+hayes-mysten@users.noreply.github.com>
2025-12-02 00:05:56 +0000

    move sui sdk to packages/sui (#697)

--- packages/docs/app/[[...slug]]/layout.tsx
+++ packages/docs/app/[[...slug]]/layout.tsx
@@ -16,7 +16,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 					{
 						title: 'Sui SDK',
 						description: 'TypeScript interfaces for Sui',
-						url: '/typescript',
+						url: '/sui',
 					},
 					{
 						title: 'BCS',
