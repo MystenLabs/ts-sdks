a5b065f699d64ecaae43c683ed3ef56819e9b086
fuyutarow <fuyutarow@gmail.com>
2025-12-02 21:30:46 +0900

    refactor: Remove unused isOptionType function and simplify argument validation in normalizeMoveArguments

--- packages/codegen/tests/generated/utils/index.ts
+++ packages/codegen/tests/generated/utils/index.ts
@@ -64,10 +64,6 @@ export function getPureBcsSchema(typeTag: string | TypeTag): BcsType<any> | null
 	return null;
 }
 
-function isOptionType(type: string) {
-	return type.includes('::option::Option<');
-}
-
 export function normalizeMoveArguments(
 	args: unknown[] | object,
 	argTypes: string[],
@@ -119,7 +115,7 @@ export function normalizeMoveArguments(
 			const name = parameterNames[index];
 			arg = args[name as keyof typeof args];
 
-			if (arg === undefined || (arg === null && !isOptionType(argTypes[i]))) {
+			if (arg === undefined) {
 				throw new Error(`Parameter ${name} is required`);
 			}
 		}
