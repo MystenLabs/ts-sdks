16472ba1a408bcc72f9149565ee6fffc9194c84e
Michael Hayes <michael.hayes@mystenlabs.com>
2025-12-10 15:29:06 -0800

    Migrate more thigns to grpc

--- packages/walrus/examples/quilt/read-quilt.ts
+++ packages/walrus/examples/quilt/read-quilt.ts
@@ -1,13 +1,13 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { getJsonRpcFullnodeUrl, SuiJsonRpcClient } from '@mysten/sui/jsonRpc';
+import { SuiGrpcClient } from '@mysten/sui/grpc';
 
 import { walrus } from '../../src/client.js';
 
-const client = new SuiJsonRpcClient({
-	url: getJsonRpcFullnodeUrl('testnet'),
+const client = new SuiGrpcClient({
 	network: 'testnet',
+	baseUrl: 'https://fullnode.testnet.sui.io:443',
 }).$extend(walrus());
 
 (async function main() {
