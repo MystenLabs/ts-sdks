c3e997e6bfc1109acb97909148fd66445647e47c
Michael Hayes <michael.hayes@mystenlabs.com>
2025-12-16 21:24:24 -0800

    remove nesting of uid fields

--- packages/kiosk/src/contracts/0x2/zklogin_verified_issuer.ts
+++ packages/kiosk/src/contracts/0x2/zklogin_verified_issuer.ts
@@ -4,12 +4,11 @@
 import { MoveStruct, normalizeMoveArguments, type RawTransactionArgument } from '../utils/index.js';
 import { bcs } from '@mysten/sui/bcs';
 import { type Transaction } from '@mysten/sui/transactions';
-import * as object from './object.js';
 const $moduleName = '0x2::zklogin_verified_issuer';
 export const VerifiedIssuer = new MoveStruct({
 	name: `${$moduleName}::VerifiedIssuer`,
 	fields: {
-		id: object.UID,
+		id: bcs.Address,
 		owner: bcs.Address,
 		issuer: bcs.string(),
 	},
