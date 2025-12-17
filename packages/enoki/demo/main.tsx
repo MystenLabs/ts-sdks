0e008d6801c0be71ffae6aef4131fd5cb2649b90
Michael Hayes <michael.hayes@mystenlabs.com>
2025-12-16 22:47:50 -0800

    update a few examples

--- packages/enoki/demo/main.tsx
+++ packages/enoki/demo/main.tsx
@@ -1,39 +1,19 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { DAppKitProvider } from '@mysten/dapp-kit-react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import '@mysten/dapp-kit/dist/index.css';
-
-import { App } from './App.tsx';
-import { RegisterEnokiWallets } from './RegisterEnokiWallets.tsx';
-import { getJsonRpcFullnodeUrl } from '@mysten/sui/jsonRpc';
-
-const queryClient = new QueryClient();
+import { App } from './App.js';
+import { RegisterEnokiWallets } from './RegisterEnokiWallets.js';
+import { dAppKit } from './dapp-kit.js';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<SuiClientProvider
-				networks={{
-					testnet: {
-						network: 'testnet',
-						url: getJsonRpcFullnodeUrl('testnet'),
-					},
-					localnet: {
-						network: 'localnet',
-						url: 'http://localhost:8000',
-					},
-				}}
-			>
-				<RegisterEnokiWallets />
-				<WalletProvider autoConnect>
-					<App />
-				</WalletProvider>
-			</SuiClientProvider>
-		</QueryClientProvider>
+		<DAppKitProvider dAppKit={dAppKit}>
+			<RegisterEnokiWallets />
+			<App />
+		</DAppKitProvider>
 	</React.StrictMode>,
 );
