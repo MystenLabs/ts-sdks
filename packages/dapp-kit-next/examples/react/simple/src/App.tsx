b47c57d35db0f3acf85f026d9948d7e2c1dc9050
William Robertson <williamrobertson@mystenlabs.com>
2026-02-20 15:01:02 -0500

    work

--- packages/dapp-kit-next/examples/react/simple/src/App.tsx
+++ packages/dapp-kit-next/examples/react/simple/src/App.tsx
@@ -4,18 +4,7 @@
 import { ConnectButton } from '@mysten/dapp-kit-react';
 
 function App() {
-	return (
-		<div style={{ display: 'flex', flexDirection: 'column', gap: '24px', padding: '40px' }}>
-			<div>
-				<h3 style={{ marginBottom: '8px' }}>Default</h3>
-				<ConnectButton />
-			</div>
-			<div>
-				<h3 style={{ marginBottom: '8px' }}>Custom ::part(trigger)</h3>
-				<ConnectButton className="custom-trigger" />
-			</div>
-		</div>
-	);
+	return <ConnectButton />;
 }
 
 export default App;
