8cb180d95262787b8b0f1659b171118ef6b82669
synasapmob <sonn51280@gmail.com>
2026-04-09 11:15:32 +0700

    update import statements to use 'type' for utils

--- packages/codegen/src/generate-utils.ts
+++ packages/codegen/src/generate-utils.ts
@@ -5,15 +5,15 @@ export const utilsContent = /* ts */ `
 import {
 	bcs,
 	BcsType,
-	TypeTag,
+	type TypeTag,
 	TypeTagSerializer,
 	BcsStruct,
 	BcsEnum,
 	BcsTuple,
 } from '@mysten/sui/bcs';
 import { normalizeSuiAddress } from '@mysten/sui/utils';
-import { TransactionArgument, isArgument } from '@mysten/sui/transactions';
-import { ClientWithCoreApi, SuiClientTypes } from '@mysten/sui/client';
+import { type TransactionArgument, isArgument } from '@mysten/sui/transactions';
+import type { ClientWithCoreApi, SuiClientTypes } from '@mysten/sui/client';
 
 const MOVE_STDLIB_ADDRESS = normalizeSuiAddress('0x1');
 const SUI_FRAMEWORK_ADDRESS = normalizeSuiAddress('0x2');
